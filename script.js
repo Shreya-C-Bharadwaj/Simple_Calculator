ea69a4f462639bd6096e452fda7a29d813277347
Shreya-C-Bharadwaj <146917090+Shreya-C-Bharadwaj@users.noreply.github.com>
2024-05-11 17:16:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Calculator program
+
+const display =document.getElementById("display");
+let answer=0;
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+   
+}
